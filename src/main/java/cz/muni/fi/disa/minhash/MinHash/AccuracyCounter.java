e4b03a067925e6555d383fd290c47d3039a091d1
jurajvandor <433451@mail.muni.cz>
2018-11-06 18:07:37 +0100

    added templates

--- src/main/java/cz/muni/fi/disa/minhash/MinHash/AccuracyCounter.java
+++ src/main/java/cz/muni/fi/disa/minhash/MinHash/AccuracyCounter.java
@@ -1,4 +1,11 @@
 package cz.muni.fi.disa.minhash.MinHash;
 
 public class AccuracyCounter {
+    private String originalDataFile;
+    private String minahshFile;
+
+    AccuracyCounter(String originalDataFile, String minhashFile){
+        this.minahshFile = minhashFile;
+        this.originalDataFile = originalDataFile;
+    }
 }
--- src/main/java/cz/muni/fi/disa/minhash/MinHash/MinhashQueryExecutor.java
+++ src/main/java/cz/muni/fi/disa/minhash/MinHash/MinhashQueryExecutor.java
@@ -1,4 +1,22 @@
 package cz.muni.fi.disa.minhash.MinHash;
 
+import cz.muni.fi.disa.minhash.DataHelpers.IntegerVectorData;
+
+import java.util.List;
+
 public class MinhashQueryExecutor {
+
+    private String minhashFile;
+
+    MinhashQueryExecutor(String minhashFile){
+        this.minhashFile = minhashFile;
+    }
+
+    public List<IntegerVectorData> findSimilarItems(int numberOfRequestedItems, String idOfQueryItem){
+        return null;
+    }
+
+    public List<IntegerVectorData> findSimilarItems(int numberOfRequestedItems, int[] queryVector){
+        return null;
+    }
 }
