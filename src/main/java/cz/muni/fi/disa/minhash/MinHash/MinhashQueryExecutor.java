249376c667e53c59409a027a55c56f5df2f5fbc3
jurajvandor <433451@mail.muni.cz>
2018-11-17 22:47:56 +0100

    base of query execution

--- src/main/java/cz/muni/fi/disa/minhash/MinHash/MinhashQueryExecutor.java
+++ src/main/java/cz/muni/fi/disa/minhash/MinHash/MinhashQueryExecutor.java
@@ -1,22 +1,48 @@
 package cz.muni.fi.disa.minhash.MinHash;
 
 import cz.muni.fi.disa.minhash.DataHelpers.IntegerVectorData;
+import cz.muni.fi.disa.minhash.DataHelpers.IntegerVectorLoader;
+import sun.awt.util.IdentityArrayList;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class MinhashQueryExecutor {
 
-    private String minhashFile;
+    private IntegerVectorLoader loader;
+    private ArrayList<IntegerVectorData> data;
 
-    MinhashQueryExecutor(String minhashFile){
-        this.minhashFile = minhashFile;
+    MinhashQueryExecutor(IntegerVectorLoader loader){
+        this.loader = loader;
+        data = loader.loadAllVectorsToArrayList();
     }
 
     public List<IntegerVectorData> findSimilarItems(int numberOfRequestedItems, String idOfQueryItem){
-        return null;
+        Optional<IntegerVectorData> query = data.stream().findFirst().filter(x -> x.getId().equals(idOfQueryItem));
+        return query.isPresent() ? findSimilarItems(numberOfRequestedItems, query.get().getVector()) : null;
     }
 
     public List<IntegerVectorData> findSimilarItems(int numberOfRequestedItems, int[] queryVector){
-        return null;
+        PriorityQueue<IntegerVectorData> result = new PriorityQueue<>(new MinhashComparator());
+        for (IntegerVectorData item : data){
+            result.add(item);
+            if (result.size() > numberOfRequestedItems)
+                result.poll();
+        }
+        return result.stream().sorted(new MinhashComparator()).collect(Collectors.toList());
+    }
+
+    public class MinhashComparator implements Comparator<IntegerVectorData> {
+
+        @Override
+        public int compare(IntegerVectorData o1, IntegerVectorData o2) {
+
+            return 0;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return obj instanceof MinhashComparator;
+        }
     }
 }
