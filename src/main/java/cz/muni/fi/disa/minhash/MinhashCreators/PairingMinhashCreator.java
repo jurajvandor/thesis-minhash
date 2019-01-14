4c822fe98524da8768440220d252af84e98e1325
jurajvandor <433451@mail.muni.cz>
2019-01-14 17:31:43 +0100

    pairing experiment creator

--- src/main/java/cz/muni/fi/disa/minhash/MinhashCreators/PairingMinhashCreator.java
+++ src/main/java/cz/muni/fi/disa/minhash/MinhashCreators/PairingMinhashCreator.java
@@ -1,9 +1,24 @@
 package cz.muni.fi.disa.minhash.MinhashCreators;
 
 import cz.muni.fi.disa.minhash.DataHolders.Loaders.BooleanVectorLoader;
+import cz.muni.fi.disa.minhash.DataHolders.ObjectData.BooleanVectorData;
+import cz.muni.fi.disa.minhash.DataHolders.PermutationException;
 import cz.muni.fi.disa.minhash.DataHolders.PermutationGenerator;
+import cz.muni.fi.disa.minhash.DataHolders.VectorLoaderException;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class PairingMinhashCreator implements MinhashCreator{
+
+    public static void main(String[] args)throws VectorLoaderException, MinhashException {
+        MinhashCreator creator = new PairingMinhashCreator(new BooleanVectorLoader("data_files/features-images-profiset100K.data", " ", 4096), 2048);
+        creator.createMinhashes();
+    }
 
     private BooleanVectorLoader loader;
     private PermutationGenerator generator;
@@ -30,6 +45,35 @@ public class PairingMinhashCreator implements MinhashCreator{
     }
     @Override
     public void createMinhashes() throws MinhashException {
-
+        String path = loader.getPath().replace(".data", "") +
+                "_minhash_3_" + generator.getNumberOfVectors() + ".data";
+        try {
+            OutputStream out = Files.newOutputStream(Paths.get(path));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
+            int[][] permutations = generator.loadPermutations();
+            for (BooleanVectorData data : loader) {
+                writer.write("#objectKey messif.objects.keys.AbstractObjectKey " + data.getId());
+                writer.newLine();
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < generator.getNumberOfVectors(); i++) {
+                    int j = 0;
+                    int index = permutations[i][j];
+                    while (!(data.getVector()[index/loader.getVectorSize()] && data.getVector()[index%loader.getVectorSize()]))
+                        j++;
+                    if (i != 0)
+                        builder.append(" ");
+                    j++;
+                    builder.append(j);
+                }
+                writer.write(builder.toString());
+                writer.newLine();
+            }
+            writer.newLine();
+            writer.close();
+        }catch (IOException e){
+            throw new MinhashException("Data of created minhash could not be written to file", e);
+        }catch (PermutationException e) {
+            throw new MinhashException("Error loading permutation", e);
+        }
     }
 }
